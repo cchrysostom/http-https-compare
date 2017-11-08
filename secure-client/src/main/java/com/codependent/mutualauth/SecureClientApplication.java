902d65998b0f6ad68868c469d511aabaee1a3ca9
Chris Chrysostom <chris.chrysostom@gmail.com>
2017-11-08 16:04:22 -0700

    Complete test with multiple pool connections

--- secure-client/src/main/java/com/codependent/mutualauth/SecureClientApplication.java
+++ secure-client/src/main/java/com/codependent/mutualauth/SecureClientApplication.java
@@ -1,20 +1,43 @@
 package com.codependent.mutualauth;
 
-import javax.annotation.PostConstruct;
-
+import ch.qos.logback.core.net.ssl.KeyStoreFactoryBean;
+import org.apache.catalina.webresources.FileResource;
+import org.apache.http.HttpHost;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.routing.HttpRoute;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContexts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import javax.net.ssl.SSLContext;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.KeyStore;
 
 @SpringBootApplication
 public class SecureClientApplication {
-
+    static public final String TRUST_STORE_FILE = "/Users/cchrysostom/github.com/codependent/spring-boot-ssl-mutual-authentication/client-truststore.jks";
+    static public final String KEYS_STORE_FILE = "/Users/cchrysostom/github.com/codependent/spring-boot-ssl-mutual-authentication/client-keystore.jks";
+    static public final int POOL_MAX = 500;
+    static public final int POOL_MAX_ROUTE = 500;
 	@PostConstruct
 	public void initSsl(){
-		System.setProperty("javax.net.ssl.keyStore", "/Users/cchrysostom/github.com/codependent/spring-boot-ssl-mutual-authentication/client-keystore.jks");
+		System.setProperty("javax.net.ssl.keyStore", KEYS_STORE_FILE);
 		System.setProperty("javax.net.ssl.keyStorePassword", "secret");
-		System.setProperty("javax.net.ssl.trustStore", "/Users/cchrysostom/github.com/codependent/spring-boot-ssl-mutual-authentication/client-truststore.jks");
+		System.setProperty("javax.net.ssl.trustStore", TRUST_STORE_FILE);
 		System.setProperty("javax.net.ssl.trustStorePassword", "secret");
 		/*
 		javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier(
@@ -26,10 +49,42 @@ public class SecureClientApplication {
 			});*/
 	}
 	
-	@Bean
+	@Bean(name="restTemplate")
 	public RestTemplate template() throws Exception{
 		RestTemplate template = new RestTemplate();
 		return template;
+	}
+
+	@Bean(name="restTemplatePool")
+    public RestTemplate restTemplatePool() throws Exception {
+        ConnectionSocketFactory plainsf = PlainConnectionSocketFactory.getSocketFactory();
+        KeyStore trustStore = KeyStore.getInstance("JKS");
+        trustStore.load(new FileInputStream(TRUST_STORE_FILE), "secret".toCharArray());
+        KeyStore keyStore = KeyStore.getInstance("JKS");
+        keyStore.load(new FileInputStream(KEYS_STORE_FILE), "secret".toCharArray());
+
+        SSLContext sslContext = SSLContexts.custom()
+                .loadTrustMaterial(trustStore, TrustSelfSignedStrategy.INSTANCE)
+                .loadKeyMaterial(keyStore, "secret".toCharArray())
+                .build();
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext);
+        Registry<ConnectionSocketFactory> r = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", plainsf)
+                .register("https", sslsf)
+                .build();
+
+        PoolingHttpClientConnectionManager poolingConnManager
+                = new PoolingHttpClientConnectionManager(r);
+        poolingConnManager.setMaxTotal(POOL_MAX);
+        poolingConnManager.setDefaultMaxPerRoute(POOL_MAX_ROUTE);
+        HttpHost localhost = new HttpHost("locahost", 8443);
+        poolingConnManager.setMaxPerRoute(new HttpRoute(localhost), POOL_MAX_ROUTE);
+
+        CloseableHttpClient client = HttpClients.custom()
+                .setConnectionManager(poolingConnManager)
+                .build();
+		RestTemplate restTemplatePool = new RestTemplate(new HttpComponentsClientHttpRequestFactory(client));
+		return restTemplatePool;
 	}
 	
 	public static void main(String[] args) {
--- secure-client/src/main/java/com/codependent/mutualauth/web/HomeRestController.java
+++ secure-client/src/main/java/com/codependent/mutualauth/web/HomeRestController.java
@@ -14,37 +14,78 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class HomeRestController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeRestController.class);
-	private static final int LOOP_COUNT = 100;
+	private static final int LOOP_COUNT = 500;
+    private static final int[] TEST_COUNTS = { 1, 500, 1000, 1500, 2000 };
 
 	@Autowired
 	private RestTemplate restTemplate;
+
+	@Autowired
+	private RestTemplate restTemplatePool;
 	
 	@GetMapping("/")
 	public String home() throws RestClientException, URISyntaxException{
-		String response = "";
-		long start = System.nanoTime();
-		for (int i=0; i<LOOP_COUNT; i++) {
-			response = restTemplate.getForObject(new URI("https://localhost:8443"), String.class);
-		}
-		long end  = System.nanoTime();
+		return tlsNoPool(LOOP_COUNT);
+	}
 
-		String m = String.format("Elapsed TLS time: %d nanoseconds.", end - start);
-		logger.info(m);
-		return String.format("Response sample: %s, %s", response, m);
+    @GetMapping("/tlspool")
+	public String tlspool() throws RestClientException, URISyntaxException {
+		return tlsPool(LOOP_COUNT);
 	}
 
 	@GetMapping("/nontls")
 	public String nontls() throws URISyntaxException {
-		String response = "";
-		long start = System.nanoTime();
-		for (int i=0; i<LOOP_COUNT; i++) {
-			response = restTemplate.getForObject(new URI("http://localhost:8400/notlsmsg"), String.class);
-		}
-		long end  = System.nanoTime();
-
-		String m = String.format("Elapsed non-TLS time: %d nano-seconds", end - start);
-		logger.info(m);
-		return String.format("Response sample: %s, %s", response, m);
+        return notls(LOOP_COUNT);
 
 	}
+
+	@GetMapping("/complete")
+    public String complete() throws URISyntaxException {
+	    StringBuilder sb = new StringBuilder();
+        for (int i=0; i<TEST_COUNTS.length; i++) {
+            sb.append(notls(TEST_COUNTS[i]));
+            sb.append(tlsNoPool(TEST_COUNTS[i]));
+            sb.append(tlsPool(TEST_COUNTS[i]));
+        }
+        return sb.toString();
+    }
+
+    public String tlsNoPool(int count) throws URISyntaxException {
+        String response = "";
+        long start = System.nanoTime();
+        for (int i=0; i<count; i++) {
+            response = restTemplate.getForObject(new URI("https://localhost:8443"), String.class);
+        }
+        long end  = System.nanoTime();
+
+        String m = String.format("TLS Single Connection. Count: %d,  Elapsed: %d nanoseconds.\n", count, end - start);
+        logger.info(m);
+        return m;
+    }
+
+    public String tlsPool(int count) throws URISyntaxException {
+        String response = "";
+        long start = System.nanoTime();
+        for (int i=0; i<count; i++) {
+            response = restTemplatePool.getForObject(new URI("https://localhost:8443"), String.class);
+        }
+        long end  = System.nanoTime();
+
+        String m = String.format("TLS Pooled Connection. Count: %d, time: %d nanoseconds.\n", count, end - start);
+        logger.info(m);
+        return m;
+    }
+
+    public String notls(int count) throws URISyntaxException {
+        String response = "";
+        long start = System.nanoTime();
+        for (int i=0; i<count; i++) {
+            response = restTemplate.getForObject(new URI("http://localhost:8400/notlsmsg"), String.class);
+        }
+        long end  = System.nanoTime();
+
+        String m = String.format("Simple HTTP Single Connection. Count: %d, Elapsed: %d nanoseconds\n", count, end - start);
+        logger.info(m);
+        return m;
+    }
 }
